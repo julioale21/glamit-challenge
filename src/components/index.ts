21e155311bfcfc08a1dbb37bcc8ae097df33c605
Julio Romero <julioale21@gmail.com>
2021-08-27 16:47:49 -0300

    exported components by default

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Cart } from "./Header/Cart";
+export { default as Header } from "./Header";
+export { default as Logo } from "./Logo";
+export { default as MenuToggle } from "./Header/MenuToggle";
