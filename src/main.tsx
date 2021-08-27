915b2275648a32f5e262982184ae40c426e95779
Julio Romero <julioale21@gmail.com>
2021-08-27 16:47:34 -0300

    extended chakra ui theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App";
+import "@fontsource/roboto";
+
+const theme = extendTheme({
+  colors: {
+    primary: "#100217E6",
+    menuText: "#FAFAFA",
+    textColor: "#030B1C",
+  },
+  fonts: {
+    body: "Roboto",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
