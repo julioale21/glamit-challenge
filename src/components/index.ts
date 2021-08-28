5bc999d6857b01d66a2fefbfb2ed429c0c695f17
Julio Romero <julioale21@gmail.com>
2021-08-28 10:10:05 -0300

    added exports components by default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
-export { default as Cart } from "./Header/Cart";
+export { default as Filter } from "./Filter";
 export { default as Header } from "./Header";
 export { default as Logo } from "./Logo";
+export { default as MenuIcons } from "./Header/MenuIcons";
 export { default as MenuToggle } from "./Header/MenuToggle";
+export { default as ProductCard } from "./ProductCard";
