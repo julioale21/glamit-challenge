3aa0e19726722296fd35fcaae87d417780951bc9
Julio Romero <julioale21@gmail.com>
2021-08-28 10:29:09 -0300

    extracted 'MenuLinks' and 'LinkItem' components and added to 'Footer'

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,20 @@
 import React from "react";
-import { Stack, Text } from "@chakra-ui/layout";
+import { Stack } from "@chakra-ui/layout";
+import MenuLinks from "./MenuLinks";
 
 const Footer: React.FC = () => {
   return (
     <Stack
       alignItems="center"
+      as="footer"
       bg="primary"
       boxShadow="0px -3px 6px #00000026"
-      direction={{ base: "column" }}
-      h="161px"
-      justifyContent="center"
+      direction={{ base: "column", lg: "row" }}
+      h={{ base: "161px", lg: "58px" }}
+      justifyContent={{ base: "center", lg: "space-between" }}
+      paddingX={{ lg: "calc((100vw - 665px) / 2)" }}
     >
-      <Text color="menuText" fontSize="16px" letterSpacing="0.32px">
-        Ayuda
-      </Text>
-      <Text color="menuText" fontSize="16px" letterSpacing="0.32px">
-        Contacto
-      </Text>
-      <Text color="menuText" fontSize="16px" letterSpacing="0.32px">
-        Términos y condiciones
-      </Text>
-      <Text color="menuText" fontSize="16px" letterSpacing="0.32px">
-        Políticas de privacidad
-      </Text>
+      <MenuLinks />
     </Stack>
   );
 };
---
+++ src/components/Footer/MenuLinks/MenuLinks.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LinkItem from "../LinkItem";
+
+const MenuLinks = () => {
+  return (
+    <>
+      <LinkItem name="Ayuda" />
+      <LinkItem name="Contacto" />
+      <LinkItem name="Términos y condiciones" />
+      <LinkItem name="Políticas de privacidad" />
+    </>
+  );
+};
+
+export default MenuLinks;
