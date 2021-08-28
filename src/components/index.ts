9b8e83e2e219376de69c0743a4c26cb3ccc2b281
Julio Romero <julioale21@gmail.com>
2021-08-28 10:29:25 -0300

    exported components by default

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as Filter } from "./Filter";
 export { default as Header } from "./Header";
+export { default as Linkitem } from "./Footer/LinkItem";
 export { default as Logo } from "./Logo";
 export { default as MenuIcons } from "./Header/MenuIcons";
+export { default as MenuLinks } from "./Footer/MenuLinks";
 export { default as MenuToggle } from "./Header/MenuToggle";
 export { default as ProductCard } from "./ProductCard";
