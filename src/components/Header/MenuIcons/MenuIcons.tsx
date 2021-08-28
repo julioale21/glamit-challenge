68298bc89894512e87a6eee8245a164b9bf453e4
Julio Romero <julioale21@gmail.com>
2021-08-28 15:03:30 -0300

    removed background to icon buttons

--- src/components/Header/MenuIcons/MenuIcons.tsx
+++ src/components/Header/MenuIcons/MenuIcons.tsx
@@ -6,18 +6,24 @@ const MenuIcons: React.FC = () => {
   return (
     <Stack direction="row" spacing={0}>
       <IconButton
+        _focus={{ border: "none" }}
+        _hover={{ background: "transparent", ring: "none" }}
         aria-label=""
         backgroundColor="transparent"
         display={{ base: "none", lg: "inline-flex" }}
         icon={<FaSearch color="white" size="27px" />}
       />
       <IconButton
+        _focus={{ border: "none" }}
+        _hover={{ background: "transparent", ring: "none" }}
         aria-label=""
         backgroundColor="transparent"
         display={{ base: "none", lg: "inline-flex" }}
         icon={<FaUser color="white" size="27px" />}
       />
       <IconButton
+        _focus={{ border: "none" }}
+        _hover={{ background: "transparent", ring: "none" }}
         aria-label=""
         backgroundColor="transparent"
         icon={<FaShoppingCart color="white" size="27px" />}
