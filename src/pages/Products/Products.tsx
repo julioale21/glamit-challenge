fb472f852321579c05ee2ebf2b7904fc0a6da84d
Julio Romero <julioale21@gmail.com>
2021-08-28 23:04:00 -0300

    added custom 'Cart' component to products view

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Box, Button, Image, Modal, ModalOverlay, Stack, Text } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import Filter from "../../components/Filter";
-import { ProductList } from "../../components";
+import { Cart, ProductList } from "../../components";
 import Product from "../../models/Product";
 import ApiService from "../../services/ApiService";
 import ProductListSkeleton from "../../components/ProductListSkeleton/ProductListSkeleton";
@@ -9,7 +9,7 @@ import { ProductContext } from "../../context/ProductContext";
 
 const Products: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const { cart, addToCart, isCartOpen, closeCart } = useContext(ProductContext);
+  const { addToCart } = useContext(ProductContext);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -20,11 +20,6 @@ const Products: React.FC = () => {
 
     fetchProducts();
   }, []);
-
-  const total = React.useMemo(
-    () => cart.reduce((total, product) => total + product.price, 0),
-    [cart],
-  );
 
   const handleSelectedProduct = (product: Product) => {
     addToCart(product);
@@ -68,86 +63,7 @@ const Products: React.FC = () => {
           <ProductListSkeleton />
         )}
       </Stack>
-      <Modal isOpen={isCartOpen} onClose={closeCart}>
-        <ModalOverlay onClose={closeCart} />
-        <Stack
-          backgroundColor="#FAFAFA"
-          border="5px solid"
-          borderColor="textColor"
-          maxH="100vh"
-          maxW={480}
-          overflowY="auto"
-          padding={4}
-          position="fixed"
-          right={0}
-          top={0}
-          width="100%"
-          zIndex={9999}
-        >
-          <Button
-            color="textColor"
-            marginLeft="auto"
-            paddingX={4}
-            paddingY={2}
-            variant="link"
-            onClick={closeCart}
-          >
-            Cerrar
-          </Button>
-          <Text color="textColor" fontSize="xl" fontWeight="bold" paddingX={4}>
-            Mis Productos
-          </Text>
-          <Box paddingY={2}>
-            {cart.map((product) => (
-              <Stack
-                key={product.id}
-                alignItems="center"
-                direction="row"
-                fontSize="xl"
-                justifyContent="space-between"
-                padding={4}
-              >
-                <Stack width="20%">
-                  <Image src={product.image} />
-                </Stack>
-                <Stack flex={1} justifyContent="space-between" paddingLeft={4}>
-                  <Text>{product.name}</Text>
-                  <Stack direction="row" justifyContent="space-between">
-                    <Stack alignItems="center" direction="row">
-                      <Text color="textColor" fontSize="sm" fontWeight="bold">
-                        Cantidad:
-                      </Text>
-                      <Stack alignItems="center" direction="row">
-                        <Button></Button>
-                        <Stack width="40px">
-                          <Text textAlign="center">10</Text>
-                        </Stack>
-                        <Button></Button>
-                      </Stack>
-                    </Stack>
-                    <Text>${product.price}</Text>
-                  </Stack>
-                </Stack>
-              </Stack>
-            ))}
-          </Box>
-          <Stack
-            alignItems="center"
-            borderTopColor="primary"
-            borderTopWidth={2}
-            direction="row"
-            justifyContent="space-between"
-            padding={4}
-          >
-            <Text color="textColor" flex={1} fontSize="3xl">
-              Total: ${total}
-            </Text>
-            <Button color="primary" fontSize="2xl" variant="link">
-              Comprar
-            </Button>
-          </Stack>
-        </Stack>
-      </Modal>
+      <Cart />
     </Stack>
   );
 };
