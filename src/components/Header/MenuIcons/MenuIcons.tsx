5a85b43d167e5fd0286ca629e37d27e13eec5fbf
Julio Romero <julioale21@gmail.com>
2021-08-28 21:54:21 -0300

    added cart conunt to icon cart

--- src/components/Header/MenuIcons/MenuIcons.tsx
+++ src/components/Header/MenuIcons/MenuIcons.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { IconButton, Stack } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { IconButton, Stack, Text } from "@chakra-ui/react";
 import { FaShoppingCart, FaUser, FaSearch } from "react-icons/fa";
+import { ProductContext } from "../../../context/ProductContext";
 
 const MenuIcons: React.FC = () => {
+  const { cart, openCart } = useContext(ProductContext);
+
   return (
     <Stack direction="row" spacing={0}>
       <IconButton
@@ -21,13 +24,23 @@ const MenuIcons: React.FC = () => {
         display={{ base: "none", lg: "inline-flex" }}
         icon={<FaUser color="white" size="27px" />}
       />
-      <IconButton
-        _focus={{ border: "none" }}
-        _hover={{ background: "transparent", ring: "none" }}
-        aria-label=""
-        backgroundColor="transparent"
-        icon={<FaShoppingCart color="white" size="27px" />}
-      />
+      <Stack position="relative">
+        <IconButton
+          _focus={{ border: "none" }}
+          _hover={{ background: "transparent", ring: "none" }}
+          aria-label=""
+          backgroundColor="transparent"
+          icon={<FaShoppingCart color="white" size="27px" />}
+          onClick={openCart}
+        />
+        <Stack position="absolute" right="25%" top="-20px">
+          {cart.length > 0 ? (
+            <Text color="white" fontWeight="bold">
+              {cart.length}
+            </Text>
+          ) : null}
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
