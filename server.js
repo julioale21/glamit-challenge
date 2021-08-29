2f9c2b1dcddf5c2408a5ae2a98c247d132600726
Julio Romero <julioale21@gmail.com>
2021-08-28 23:54:15 -0300

    created json-server

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./data/db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
+
+const PORT = 8000;
+
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  }),
+);
+server.use(router);
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log("Server is runnig");
+});
