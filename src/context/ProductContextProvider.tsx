ec355fdecb39765e02513055124a040d10b6621c
Julio Romero <julioale21@gmail.com>
2021-08-28 23:04:48 -0300

    updated context function params

--- src/context/ProductContextProvider.tsx
+++ src/context/ProductContextProvider.tsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import Product from "../models/Product";
 import { ProductContext } from "./ProductContext";
+import { ICart } from "./ProductContextState";
+import { editCart } from "./selector";
 
 const ProductContextProvider: React.FC = ({ children }) => {
-  const [cart, setCart] = useState<Product[]>([]);
+  const [cart, setCart] = useState<ICart[]>([]);
   const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
 
   const addToCart = (product: Product) => {
-    setCart(cart.concat(product));
+    // setCart(cart.concat(product));
+    setCart(editCart(product, "increment"));
   };
 
-  const removeFromCart = (id: number) => {
-    setCart(cart.filter((product) => product.id != id));
+  const removeFromCart = (product: Product) => {
+    //setCart(cart.filter((product) => product.id != id));
+    setCart(editCart(product, "decrement"));
   };
 
   const openCart = () => {
--- src/context/ProductContextState.ts
+++ src/context/ProductContextState.ts
@@ -1,13 +1,13 @@
 import Product from "../models/Product";
 
-export interface Icart extends Product {
+export interface ICart extends Product {
   quantity: number;
 }
 
 export type ProductContextState = {
-  cart: Product[];
+  cart: ICart[];
   addToCart: (product: Product) => void;
-  removeFromCart: (id: number) => void;
+  removeFromCart: (product: Product) => void;
   isCartOpen: boolean;
   openCart: () => void;
   closeCart: () => void;
