006459df7d9fae2661f99834094ebbf166d18fd3
Julio Romero <julioale21@gmail.com>
2021-08-28 21:53:53 -0300

    added product card styles and transition

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Image, Stack, Text } from "@chakra-ui/react";
+import { Image, IconButton, Stack, Text } from "@chakra-ui/react";
+import { FaCartPlus } from "react-icons/fa";
+import "./styles.css";
 
 interface Product {
   name: string;
@@ -8,12 +10,13 @@ interface Product {
 }
 
 interface Props {
+  onProductSelected: () => void;
   product: Product;
 }
 
-const ProductCard: React.FC<Props> = ({ product }) => {
+const ProductCard: React.FC<Props> = ({ product, onProductSelected }) => {
   return (
-    <Stack className="product-card" marginBottom="4em">
+    <Stack className="product-card" marginBottom="4em" position="relative">
       <Stack borderBottom="1.3px solid" borderColor="primary">
         <Image src={product.image} />
       </Stack>
@@ -22,6 +25,27 @@ const ProductCard: React.FC<Props> = ({ product }) => {
         <Text fontSize="20px" fontWeight="bold">
           ${product.price}
         </Text>
+      </Stack>
+
+      <Stack
+        background="rgba(0,0,0,0.3)"
+        className="add-cart"
+        direction="row"
+        height="100%"
+        justifyContent="flex-end"
+        left={0}
+        position="absolute"
+        top={0}
+        width="100%"
+      >
+        <IconButton
+          _active={{ bg: "transparent ", ring: "none" }}
+          _hover={{ bg: "transparent ", ring: "none" }}
+          aria-label=""
+          backgroundColor="transparent"
+          icon={<FaCartPlus color="white" size={30} />}
+          onClick={onProductSelected}
+        />
       </Stack>
     </Stack>
   );
